e4a8a6bbbbf781562a13643a897365ad883243f0
Jiaqi Zhao <67219207+LX-Vincent@users.noreply.github.com>
2022-07-29 10:14:23 +0800

    Update api.py

--- api.py
+++ api.py
@@ -8,6 +8,17 @@ app.config.from_object(config)
 @app.route('/')
 def index():
     return render_template('index.html')
+ 
+ 
+@app.route('/login_display', methods=['POST', 'GET'])
+def sign_in_display():
+    return render_template('login.html')
+  
+ 
+ @app.route('/register_display', methods=['POST', 'GET'])
+def sign_up_display():
+    return render_template('register.html')
+ 
 
 if __name__ == '__main__':
     app.run()
