ff69d0b4f812567320eeb2bb66ebc50896a7afa2
LiFangYuBIT <102638937+LiFangYuBIT@users.noreply.github.com>
2022-08-01 13:12:03 +0800

    Signup
    Signup Draft

--- app_pkg/routes.py
+++ app_pkg/routes.py
@@ -19,6 +19,6 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/signup', methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    return render_template('signup.html')
