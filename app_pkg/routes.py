e913ce77f660fcf65f1fdad4ea986e109a95f861
Jiaqi Zhao <vincent0110@126.com>
2022-08-10 01:07:55 +0800

    Update the routes and add login function

--- app_pkg/routes.py
+++ app_pkg/routes.py
@@ -3,13 +3,17 @@ from flask import render_template, request
 from app_pkg import app
 import pymysql
 import traceback
+from app_pkg.userLogin import userLogin
 
-#导入弹出警告框模块
+# 导入弹出警告框模块
 
 # import config
 
 # app = Flask(__name__)
 # app.config.from_object(config)
+
+# Define two global variables to store the user's name and user's id after user login this system.
+global this_user_name, this_user_id
 
 
 @app.route('/')
@@ -29,12 +33,34 @@ def login():
 
 @app.route('/login_check', methods=['POST', 'GET'])
 def login_check():
-    user_name = request.form['username']
+    global this_user_name, this_user_id
+    email = request.form['email']
     password = request.form['password']
-    if password == '' or user_name == '':
-        return render_template('signup.html')
+
+    exist_flag = userLogin().check_exist(email)
+    if exist_flag:
+        # account_type = True --> this is a student account
+        # account_type = False --> this is a lecturer account
+        account_type = userLogin().check_type(email)
+        if account_type:
+            student_result = userLogin().student_login(email, password)
+            if student_result == -1:
+                return render_template('login.html', tips='Wrong password, please check.')
+            else:
+                this_user_id = student_result[0]
+                this_user_name = student_result[1]
+                return render_template('student_main.html', user=this_user_name)
+        else:
+            userLogin().lecturer_login(email, password)
+            lecturer_result = userLogin().student_login(email, password)
+            if lecturer_result == -1:
+                return render_template('login.html', tips='Wrong password, please check.')
+            else:
+                this_user_id = lecturer_result[0]
+                this_user_name = lecturer_result[1]
+                return render_template('lecturer_main.html', user=this_user_name)
     else:
-        return
+        return render_template('login.html', tips='User not exist, please check the input or register.')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -42,11 +68,15 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/quizstudent', methods=['POST', 'GET'])
-def quizstudent():
-    return render_template('quizstudent.html')
+@app.route('/quiz_student', methods=['POST', 'GET'])
+def quiz_student():
+    return render_template('quiz_student.html')
 
 
-# @app.route('/quizlist', methods=['POST', 'GET'])
-# def quizlist():
-#     return render_template('quizlist.html')
+@app.route('/student_main')
+def student_main():
+    return render_template('student_page.html')
+
+# @app.route('/quiz_list', methods=['POST', 'GET'])
+# def quiz_list():
+#     return render_template('quiz_list.html')
