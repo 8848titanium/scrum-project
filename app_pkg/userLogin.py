5306e55887d756c123cc2a6de6ce53d46cecea16
Yue Cao <yue.cao@my.jcu.edu.au>
2022-08-11 00:05:20 +0800

    Solve the problem that users cannot login.

--- app_pkg/userLogin.py
+++ app_pkg/userLogin.py
@@ -9,28 +9,31 @@ class userLogin:
     def check_exist(self, email):
         self.email = email
         string1 = "select student_email from student;"
-        student_email = conndb().conn_one(string1)
+        student_email = conndb().conn_mul(string1)
+        print(student_email)
         string2 = "select lecturer_email from lecturer;"
-        lecturer_email = conndb().conn_one(string2)
-        if string1 is None or string2 is None:
+        lecturer_email = conndb().conn_mul(string2)
+        if student_email is None and lecturer_email is None:
             return False
         else:
             for i in student_email:
-                for j in lecturer_email:
-                    if self.email == str(i) or self.email == str(j):
-                        return True
+                if self.email == str(i[0]):
+                    return True
+            for i in lecturer_email:
+                if self.email == str(i[0]):
+                    return True
 
     def check_type(self, email):
         self.email = email
         string1 = "select student_email from student;"
-        student_email = conndb().conn_one(string1)
+        student_email = conndb().conn_mul(string1)
         string2 = "select lecturer_email from lecturer;"
-        lecturer_email = conndb().conn_one(string2)
+        lecturer_email = conndb().conn_mul(string2)
         for i in student_email:
-            if self.email == str(i):
+            if self.email == str(i[0]):
                 return True
         for j in lecturer_email:
-            if self.email == str(i):
+            if self.email == str(j[0]):
                 return False
 
     # student login
