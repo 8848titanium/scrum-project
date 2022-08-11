096ab9fa42f981df09bdeafae32fe12c93414b9b
CWJ <weijun.chew@my.jcu.edu.au>
2022-08-11 15:19:30 +0800

    student quiz page(html,css,js)

--- app_pkg/static/js/script.js
+++ app_pkg/static/js/script.js
@@ -1,3 +1,33 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "question",
+    answer: "option1",
+    options: [
+      "option1",
+      "option2",
+      "option3",
+      "option4"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
+
 //selecting all required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
