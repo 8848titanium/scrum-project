20175b3b6c7a380722094b4b60b1a56a2c22704b
8848titanium <81619114+8848titanium@users.noreply.github.com>
2022-09-04 14:57:01 +0800

    Minor adjustments & requirements update

--- api.py
+++ api.py
@@ -12,4 +12,4 @@ app = app
 socketio = socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', debug=True)
